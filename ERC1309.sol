f23756b7f49424b90972e6aab9e9db6de9dead48
jaebooker <jaesonbooker@gmail.com>
2019-12-13 16:09:13 -0800

    added new tokens

---
+++ ERC1309.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "ERC20Capped.sol";
+
+contract ERC1309 is ERC20Capped {
+
+}
---
+++ ERC1309VotingToken.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "ERC1309.sol";
+
+contract ERC1309VotingToken is ERC1309 {
+
+}
