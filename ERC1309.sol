82e5cf5cc52e6286aabd1ef3c346d7f8540f7477
jaebooker <jaesonbooker@gmail.com>
2019-12-14 18:46:34 -0800

    added make proposal

--- ERC1309.sol
+++ ERC1309.sol
@@ -2,14 +2,30 @@ pragma solidity ^0.5.0;
 
 import "ERC20Capped.sol";
 
-contract ERC1309 is ERC20Capped {
+contract ERC1309 is ERC20Capped, ERC1309VotingToken {
 
-    address[] public totalAddresses;
+    address[] private _totalAddresses;
+
+    struct Proposal {
+        address author;
+        string title;
+        string description;
+    }
+
+    mapping(address => Proposal) private _proposals;
 
     function _mint(address account, uint256 value) internal {
         require(totalSupply().add(value) <= _cap, "ERC20Capped: cap exceeded");
         super._mint(account, value);
         totalAddresses.push(account);
+    }
+
+    function makeProposal(address author, string title, string description) {
+        require(_balances[author] > 0);
+        var proposal = _proposals[author];
+        proposal.author = author;
+        proposal.title = title;
+        proposal.description = description;
     }
 
 }
