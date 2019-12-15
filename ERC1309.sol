7f6e6f60c808ae38fef83ee3a03e5ad19d58b77d
jaebooker <jaesonbooker@gmail.com>
2019-12-14 18:21:12 -0800

    added address array to mint

--- ERC1309.sol
+++ ERC1309.sol
@@ -4,4 +4,12 @@ import "ERC20Capped.sol";
 
 contract ERC1309 is ERC20Capped {
 
+    address[] public totalAddresses;
+
+    function _mint(address account, uint256 value) internal {
+        require(totalSupply().add(value) <= _cap, "ERC20Capped: cap exceeded");
+        super._mint(account, value);
+        totalAddresses.push(account);
+    }
+
 }
--- ERC1309VotingToken.sol
+++ ERC1309VotingToken.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.0;
 
-import "ERC1309.sol";
+import "ERC721Mintable.sol";
+import "ERC721Burnable.sol";
 
 contract ERC1309VotingToken is ERC721Mintable, ERC721Burnable {
-
+    function() {
+        
+    }
 }
