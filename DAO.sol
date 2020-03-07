57cf742f50c9052c9068491e94ecdf41acbb284f
jaebooker <jaesonbooker@gmail.com>
2020-03-07 15:07:31 -0800

    adding dao and admin

---
+++ DAO.sol
@@ -0,0 +1,28 @@
+import "./admin.sol";
+
+contract Congress is admin {
+
+    //setup
+    function Congress() payable {
+
+    }
+
+    //add member
+    function addMember(address targetMember, string memberName) onlyAdmin {
+
+    }
+
+    function removeMember(address targetMember) onlyAdmin {
+
+    }
+
+    //change rule
+    function changeVotingRules() {
+
+    }
+
+    //create proposal
+    function newProposal() {
+        
+    }
+}
---
+++ admin.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract admin {
+
+    address public admin;
+
+    function admin() {
+        admin = msg.sender;
+    }
+
+    modifer onlyAdmin() {
+        if(msg.sender !+ admin) throw;
+        _;
+    }
+
+}
